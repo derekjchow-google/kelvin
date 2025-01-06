11fe973d8d4afb474bd56eb6f01fe2e23226892a
Alex Van Damme <atv@google.com>
2025-01-06 14:56:56 -0800

    Set AxSIZE nicely for txns less than the bus width
    - Emit the correct size in AXI transactions that are smaller than our
      bus width (e.g. 1/2/4 for byte/short/word txns) -- previously all
      transactions were full bus width, regardless of the actual content or
      reason for transaction.
    
    Bypass-Presubmit-Reason: Cherry-pick
    
    Change-Id: I9ab9739ecbafea99c7417e9cda139c6156c38551
    (cherry picked from commit 322bf89f1b9a249abf941e9e93f2cb8ba97069fa)

--- tests/verilator_sim/kelvin/core_mini_axi_sim.cc
+++ tests/verilator_sim/kelvin/core_mini_axi_sim.cc
@@ -24,6 +24,7 @@
 #include "absl/flags/usage.h"
 #include "absl/log/check.h"
 #include "absl/log/log.h"
+#include "tests/systemc/Xbar.h"
 #include "tests/verilator_sim/elf.h"
 #include "tests/verilator_sim/sysc_tb.h"
 #include "tests/verilator_sim/util.h"
@@ -70,111 +71,6 @@ struct DebugIO {
   sc_signal<sc_bv<32>> inst_1;
   sc_signal<sc_bv<32>> inst_2;
   sc_signal<sc_bv<32>> inst_3;
-};
-
-// "Crossbar" containing a memory and a UART.
-class Xbar : sc_core::sc_module {
- public:
-  Xbar(sc_core::sc_module_name name) : sc_core::sc_module(std::move(name)) {
-    memset(memory_, 0xa5, kMemorySizeBytes);
-    socket_.register_b_transport(this, &Xbar::b_transport);
-  }
-
-  tlm_utils::simple_target_socket<Xbar>& socket() { return socket_; }
-
-  void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay) {
-    sc_dt::uint64 addr = trans.get_address();
-    unsigned int len = trans.get_data_length();
-
-    if (((addr & kMemoryAddr) == kMemoryAddr) &&
-        (addr + len < kMemoryAddr + kMemorySizeBytes)) {
-      memory_b_transport_(trans, delay);
-    } else if ((addr & kUartAddr) == kUartAddr) {
-      uart_b_transport_(trans, delay);
-    } else {
-      trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
-      return;
-    }
-  }
-
- private:
-  void memory_b_transport_(tlm::tlm_generic_payload& trans, sc_time& delay) {
-    sc_dt::uint64 addr = trans.get_address() & ~kMemoryAddr;
-    unsigned char* ptr = trans.get_data_ptr();
-    unsigned int len = trans.get_data_length();
-    unsigned int streaming_width = trans.get_streaming_width();
-    unsigned int be_len = trans.get_byte_enable_length();
-    if (streaming_width == 0) {
-      streaming_width = len;
-    }
-    if (be_len || streaming_width != len) {
-      trans.set_response_status(tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE);
-      SC_REPORT_FATAL("Memory", "BE unsupported\n");
-      return;
-    } else {
-      if ((addr + len) > sc_dt::uint64(kMemorySizeBytes)) {
-        trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
-        SC_REPORT_FATAL("Memory", "Bad address\n");
-        return;
-      }
-
-      if (trans.is_read()) {
-        memcpy(ptr, memory_ + addr, len);
-      } else if (trans.is_write()) {
-        memcpy(memory_ + addr, ptr, len);
-      } else {
-        trans.set_response_status(tlm::TLM_GENERIC_ERROR_RESPONSE);
-        SC_REPORT_FATAL("Memory", "Bad command\n");
-        return;
-      }
-    }
-  }
-
-  void uart_b_transport_(tlm::tlm_generic_payload& trans, sc_time& delay) {
-    sc_dt::uint64 addr = trans.get_address() & ~kUartAddr;
-    unsigned char* ptr = trans.get_data_ptr();
-    unsigned int len = trans.get_data_length();
-    unsigned int streaming_width = trans.get_streaming_width();
-    unsigned char* be = trans.get_byte_enable_ptr();
-    unsigned int be_len = trans.get_byte_enable_length();
-    if (streaming_width == 0) {
-      streaming_width = len;
-    }
-
-    if (be_len || streaming_width != len) {
-      for (unsigned int pos = 0; pos < len / 8; ++pos) {
-        bool do_access = true;
-        if (be_len) {
-          do_access = be[pos % (be_len / 8)] == TLM_BYTE_ENABLED;
-        }
-        if (do_access) {
-          if (addr != 0 || trans.is_read()) {
-            trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
-            SC_REPORT_FATAL("Uart", "Unsupported access\n");
-            return;
-          }
-          if (ptr[pos] == '\n') {
-            uart_buffer_.push_back('\0');
-            LOG(INFO) << uart_buffer_.data();
-            uart_buffer_.clear();
-          } else {
-            uart_buffer_.push_back(ptr[pos]);
-          }
-        }
-      }
-    } else {
-      trans.set_response_status(tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE);
-      SC_REPORT_FATAL("Uart", "Non-BE unsupported\n");
-      return;
-    }
-  }
-
-  static constexpr uint32_t kMemoryAddr = 0x20000000;
-  static constexpr size_t kMemorySizeBytes = 0x400000;
-  static constexpr uint32_t kUartAddr = 0x54000000;
-  uint8_t memory_[kMemorySizeBytes];
-  std::vector<char> uart_buffer_;
-  tlm_utils::simple_target_socket<Xbar> socket_;
 };
 
 struct CoreMiniAxi_tb : Sysc_tb {
