49d9fe0ec1d13a298062987fecd3f7537811ddd5
Alex Van Damme <atv@google.com>
2025-02-06 16:33:18 -0800

    Update DBus2Axi logic
    - Use queue-based system for control (both directions) and write data
    - Split reads that would cross a 4k boundary into multiple bursts
    - Split unaligned writes into multiple bursts
    
    Bypass-Presubmit-Reason: Cherry-pick
    
    Change-Id: Ie27461a5629be13ed603ca0c61b915270e080399
    (cherry picked from commit e19fa2dd3ffcf0337ef7b316d7fc6787152f5234)

--- tests/verilator_sim/kelvin/dbus2axi_tb.cc
+++ tests/verilator_sim/kelvin/dbus2axi_tb.cc
@@ -35,6 +35,11 @@ struct DBus2Axi_tb : Sysc_tb {
   sc_out<sc_bv<256> > io_dbus_wdata;
   sc_out<sc_bv<32> > io_dbus_wmask;
   sc_in<sc_bv<256> > io_dbus_rdata;
+  sc_in<sc_bv<32> > io_dbus_pc;
+  sc_in<bool> io_fault_valid;
+  sc_in<bool> io_fault_bits_write;
+  sc_in<sc_bv<32> > io_fault_bits_addr;
+  sc_in<sc_bv<32>> io_fault_bits_epc;
   sc_in<sc_bv<32> > io_axi_write_addr_bits_addr;
   sc_in<sc_bv<6> > io_axi_write_addr_bits_id;
   sc_in<sc_bv<4> > io_axi_write_addr_bits_region;
@@ -73,7 +78,7 @@ struct DBus2Axi_tb : Sysc_tb {
     for (int i = 0; i < 8; ++i) dbus_wdata.set_word(i, rand_uint32());
     dbus_wmask.set_word(0, rand_uint32());
 
-    if (!(io_dbus_valid && !io_dbus_ready && !io_dbus_write)) {
+    if (!io_dbus_valid || io_dbus_ready) {
       io_dbus_valid = rand_bool();
       io_dbus_write = rand_bool();
       io_dbus_addr = rand_uint32();
@@ -94,16 +99,17 @@ struct DBus2Axi_tb : Sysc_tb {
     if (io_dbus_valid && !io_dbus_write && !dbus_read_ready_) {
       dbus_read_ready_ = true;
       axi_read_addr_t r;
-      r.addr = io_dbus_addr.read().get_word(0) & ~31;
+      r.addr = io_dbus_addr.read().get_word(0);
       r.id = 0x00;  // from RTL
       axi_read_addr_.write(r);
     }
 
-    if (io_dbus_valid && io_dbus_ready && io_dbus_write) {
+    if (io_dbus_valid && io_dbus_write && !dbus_write_active_) {
+      dbus_write_active_ = true;
       axi_write_addr_t w;
       sc_bv<256> data;
       sc_bv<32> strb;
-      w.addr = io_dbus_addr.read().get_word(0) & ~31;
+      w.addr = io_dbus_addr.read().get_word(0);
       w.id = 0x00;  // from RTL
       w.strb = io_dbus_wmask;
       w.data = io_dbus_wdata;
@@ -114,6 +120,7 @@ struct DBus2Axi_tb : Sysc_tb {
     // DBus Read Data.
     if (dbus_read_active_) {
       dbus_read_active_ = false;
+      axi_read_fired_ = false;
       dbus_read_data_t ref, dut;
       check(dbus_read_data_.read(ref), "dbus read data");
       dut.data = io_dbus_rdata;
@@ -129,9 +136,15 @@ struct DBus2Axi_tb : Sysc_tb {
       dbus_read_active_ = true;
     }
 
+    if (io_dbus_valid && io_dbus_ready && io_dbus_write) {
+      dbus_write_active_ = false;
+      dbus_write_resp_phase_ = false;
+    }
+
     // *************************************************************************
     // AXI Read Addr.
-    if (io_axi_read_addr_valid && io_axi_read_addr_ready) {
+    if (io_axi_read_addr_valid && io_axi_read_addr_ready && !axi_read_fired_) {
+      axi_read_fired_ = true;
       axi_read_addr_t dut, ref;
       check(axi_read_addr_.read(ref), "axi read addr");
       dut.addr = io_axi_read_addr_bits_addr.read().get_word(0);
@@ -170,7 +183,7 @@ struct DBus2Axi_tb : Sysc_tb {
 
     // *************************************************************************
     // AXI Write Addr.
-    if (io_axi_write_addr_valid && io_axi_write_addr_ready) {
+    if (io_axi_write_addr_valid && io_axi_write_addr_ready && !dbus_write_resp_phase_) {
       assert(io_axi_write_data_valid && io_axi_write_data_ready);
       axi_write_addr_t dut, ref;
       check(axi_write_addr_.read(ref), "axi write addr");
@@ -188,6 +201,7 @@ struct DBus2Axi_tb : Sysc_tb {
       resp.id = dut.id;
       resp.resp = rand_int();
       axi_write_resp_.write(resp);
+      dbus_write_resp_phase_ = true;
     }
 
     // *************************************************************************
@@ -288,6 +302,9 @@ struct DBus2Axi_tb : Sysc_tb {
 
   bool dbus_read_ready_ = false;
   bool dbus_read_active_ = false;
+  bool dbus_write_active_ = false;
+  bool dbus_write_resp_phase_ = false;
+  bool axi_read_fired_ = false;
   fifo_t<axi_read_addr_t> axi_read_addr_;
   fifo_t<axi_read_data_t> axi_read_data_;
   fifo_t<axi_write_addr_t> axi_write_addr_;
@@ -315,6 +332,11 @@ static void DBus2Axi_test(char* name, int loops, bool trace) {
   sc_signal<sc_bv<256> > io_dbus_wdata;
   sc_signal<sc_bv<32> > io_dbus_wmask;
   sc_signal<sc_bv<256> > io_dbus_rdata;
+  sc_signal<sc_bv<32> > io_dbus_pc;
+  sc_signal<bool> io_fault_valid;
+  sc_signal<bool> io_fault_bits_write;
+  sc_signal<sc_bv<32> > io_fault_bits_addr;
+  sc_signal<sc_bv<32> > io_fault_bits_epc;
   sc_signal<sc_bv<32> > io_axi_write_addr_bits_addr;
   sc_signal<sc_bv<6> > io_axi_write_addr_bits_id;
   sc_signal<sc_bv<4> > io_axi_write_addr_bits_region;
@@ -350,6 +372,10 @@ static void DBus2Axi_test(char* name, int loops, bool trace) {
 
   d2a.clock(tb.clock);
   d2a.reset(tb.reset);
+  BIND2(tb, d2a, io_fault_valid);
+  BIND2(tb, d2a, io_fault_bits_epc);
+  BIND2(tb, d2a, io_fault_bits_addr);
+  BIND2(tb, d2a, io_fault_bits_write);
   BIND2(tb, d2a, io_dbus_valid);
   BIND2(tb, d2a, io_dbus_ready);
   BIND2(tb, d2a, io_dbus_write);
@@ -369,6 +395,7 @@ static void DBus2Axi_test(char* name, int loops, bool trace) {
   BIND2(tb, d2a, io_dbus_wdata);
   BIND2(tb, d2a, io_dbus_wmask);
   BIND2(tb, d2a, io_dbus_rdata);
+  BIND2(tb, d2a, io_dbus_pc);
   BIND2(tb, d2a, io_axi_write_addr_bits_addr);
   BIND2(tb, d2a, io_axi_write_addr_bits_id);
   BIND2(tb, d2a, io_axi_write_addr_bits_region);
